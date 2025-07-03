7ae1be7c232350ea267ef347585f1652a0d271b2
Mateusz Statkiewicz <104099101+Keitar6@users.noreply.github.com>
2025-07-03 14:20:10 +0200

    feat(ui): rebase to origin

--- src/app/ai-knowledge-vector-db/pages/ai-knowledge-vector-db-details/ai-knowledge-vector-db-details.actions.ts
+++ src/app/ai-knowledge-vector-db/pages/ai-knowledge-vector-db-details/ai-knowledge-vector-db-details.actions.ts
@@ -1,5 +1,5 @@
-import { createActionGroup, props } from '@ngrx/store'
-import { AIKnowledgeVectorDb } from '../../../shared/generated'
+import { createActionGroup, emptyProps, props } from '@ngrx/store'
+import { AIContext, AIKnowledgeVectorDb } from '../../../shared/generated'
 
 export const AIKnowledgeVectorDbDetailsActions = createActionGroup({
   source: 'AIKnowledgeVectorDbDetails',
@@ -10,6 +10,39 @@ export const AIKnowledgeVectorDbDetailsActions = createActionGroup({
     'ai knowledge vector db details received': props<{
       details: AIKnowledgeVectorDb
     }>(),
-    'ai knowledge vector db details loading failed': props<{ error: string | null }>()
+    'ai knowledge vector db details loading failed': props<{ error: string | null }>(),
+    'ai knowledge vector db contexts received': props<{
+      contexts: AIContext[]
+    }>(),
+    'ai knowledge vector db contexts loading failed': props<{ error: string | null }>(),
+
+    'Update AIKnowledge vector db cancelled': emptyProps(),
+    'Update AIKnowledge vector db succeeded': emptyProps(),
+    'Update AIKnowledge vector db failed': props<{
+      error: string | null
+    }>(),
+    'Delete AIKnowledge vector db cancelled': emptyProps(),
+    'Delete AIKnowledge vector db succeeded': emptyProps(),
+    'Delete AIKnowledge vector db failed': props<{
+      error: string | null
+    }>(),
+    'cancel edit back clicked': emptyProps(),
+    'cancel edit confirm clicked': emptyProps(),
+    'cancel edit not dirty': emptyProps(),
+
+    'edit mode set': props<{ editMode: boolean }>(),
+    'edit button clicked': emptyProps(),
+    'save button clicked': props<{
+      details: AIKnowledgeVectorDb
+    }>(),
+    'cancel button clicked': props<{
+      dirty: boolean
+    }>(),
+    'delete button clicked': emptyProps(),
+    'navigate back button clicked': emptyProps(),
+    'back navigation started': emptyProps(),
+    'back navigation failed': emptyProps(),
+    'navigation to search started': emptyProps(),
+    'navigation to search not started': emptyProps()
   }
 })
