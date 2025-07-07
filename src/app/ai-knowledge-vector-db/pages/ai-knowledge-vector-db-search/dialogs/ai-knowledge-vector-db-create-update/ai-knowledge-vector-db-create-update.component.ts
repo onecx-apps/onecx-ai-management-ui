40c77da8f3869cf67417a450819e33071097621b
Mateusz Statkiewicz <mateusz.statkiewicz@capgemini.com>
2025-07-07 11:57:16 +0200

    feat(ui): update

--- src/app/ai-knowledge-vector-db/pages/ai-knowledge-vector-db-search/dialogs/ai-knowledge-vector-db-create-update/ai-knowledge-vector-db-create-update.component.ts
+++ src/app/ai-knowledge-vector-db/pages/ai-knowledge-vector-db-search/dialogs/ai-knowledge-vector-db-create-update/ai-knowledge-vector-db-create-update.component.ts
@@ -30,10 +30,10 @@ export class AIKnowledgeVectorDbCreateUpdateComponent
 
   constructor() {
     this.formGroup = new FormGroup({
-      name: new FormControl('null', [Validators.maxLength(255)]),
-      description: new FormControl('null', [Validators.maxLength(255)]),
-      vdb: new FormControl('null', [Validators.maxLength(255)]),
-      vdbCollection: new FormControl('null', [Validators.maxLength(255)])
+      name: new FormControl('', [Validators.maxLength(255)]),
+      description: new FormControl('', [Validators.maxLength(255)]),
+      vdb: new FormControl('', [Validators.maxLength(255)]),
+      vdbCollection: new FormControl('', [Validators.maxLength(255)])
     })
     this.formGroup.statusChanges
       .pipe(
